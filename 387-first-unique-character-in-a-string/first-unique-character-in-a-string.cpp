bd54a1e4d3ca46d76b08616360bee91bf38d9b79
Akhil Vettical <107746909+AkhilVettical@users.noreply.github.com>
2024-02-05 21:03:00 +0530

    Time: 34 ms (51.74%) | Memory: 13.3 MB (10.77%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -9,10 +9,8 @@ public:
         for(int i=0;i<s.length();i++)
          charmap[s[i]]++;       
         for(int i=0;i<s.length();i++)
-        {
           if(charmap[s[i]]==1)
            return i;
-        }
         return -1;
     }
 };
